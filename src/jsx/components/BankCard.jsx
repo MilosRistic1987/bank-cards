9bc3b21f2708b2a169aac7fe89622f012c1ee747
MilosRistic1987 <risticmilos1987@gmail.com>
2020-10-24 10:06:42 +0200

    app structure added

---
+++ src/jsx/components/BankCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BankCard = () => {
+    return (
+        <div>
+            bankCard
+        </div>
+    )
+}
+
+export default BankCard
---
+++ src/jsx/components/CradForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CradForm = () => {
+    return (
+        <div>
+            CradForm
+        </div>
+    )
+}
+
+export default CradForm
